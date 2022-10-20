6616c523d0203013f198a1a0aa546c34a6dd6850
alex <alex.von.schulz@web.de>
2022-10-20 12:48:16 +0200

    HomeWork_19.09.2022_2

---
+++ HomeWork_19.09.2022_2/src/Main.java
@@ -0,0 +1,34 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(searchUniqueChar("leetcode"));
+        System.out.println(searchUniqueChar("loveleetcode"));
+        System.out.println(searchUniqueChar("aabb"));
+    }
+
+    private static int searchUniqueChar(String s) {
+        int index = -1;
+        int length = s.length();
+        for (int i = 0; i < length; i++) {
+            char c1 = s.charAt(i);
+            int j;
+            for (j = 0; j < length; j++) {
+                // Нужно проверить, что мы не проверяем символ сам
+                // с собой
+                if (i == j) {
+                    continue;
+                }
+                char c2 = s.charAt(j);
+                if (c1 == c2) {
+                    // Символ с1 не уникальный
+                    break;
+                }
+            }
+            if (j == length) {
+                return i;
+            }
+        }
+        return index;
+    }
+
+}
---
+++ HomeWork_19.09.2022_2/src/Main2.java
@@ -0,0 +1,77 @@
+public class Main2 {
+
+    public static void main(String[] args) {
+        System.out.println(searchUniqueChar2("leetcode"));
+        System.out.println(searchUniqueChar2("loveleetcod"));
+        System.out.println(searchUniqueChar2("aabb"));
+    }
+
+    private static int searchUniqueChar(String s) {
+        int index = -1;
+        int length = s.length();
+        for (int i = 0; i < length; i++) {
+            char c1 = s.charAt(i);
+            int j;
+            for (j = 0; j < length; j++) {
+                // Нужно проверить, что мы не проверяем символ сам
+                // с собой
+                if (i == j) {
+                    continue;
+                }
+                char c2 = s.charAt(j);
+                if (c1 == c2) {
+                    // Символ с1 не уникальный
+                    break;
+                }
+            }
+            if (j == length) {
+                return i;
+            }
+        }
+        return index;
+    }
+
+    private static int searchUniqueChar2(String s) {
+        int index = -1;
+        int length = s.length();
+        for (int i = 0; i < length; i++) {
+            char c1 = s.charAt(i);
+            boolean isUnique = true;
+            for (int j = 0; j < length; j++) {
+                // Нужно проверить, что мы не проверяем символ сам
+                // с собой
+                if (i == j) {
+                    continue;
+                }
+                char c2 = s.charAt(j);
+                if (c1 == c2) {
+                    // Символ с1 не уникальный
+                    isUnique = false;
+                    break;
+                }
+            }
+            if (isUnique) {
+                return i;
+            }
+        }
+        return index;
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
