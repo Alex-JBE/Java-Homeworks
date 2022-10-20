6bb48ed7667087ce7f31cdec307d1969e3d8a434
alex <alex.von.schulz@web.de>
2022-10-20 12:31:06 +0200

    Homework.26.05.2022

---
+++ Homework.26.05.2022/src/firstMain.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class firstMain {
+    public static void main(String[] args) {
+        // Напишите метод, вычисляющий выражение a * (b + (c / d))
+        // и возвращающий результат,
+        // где a, b, c, d — целочисленные входные параметры этого метода.
+        Scanner scanner = new Scanner(System.in);
+        int a, b, c, d;
+        System.out.println("Input a");
+        System.out.println("Input b");
+        System.out.println("Input c");
+        System.out.println("Input d");
+
+    }
+}
